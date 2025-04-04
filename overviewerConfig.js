a887256ea721fc4ea29b490a6bdfe64e3f3c4905
Christopher Janssen <saintlorraine1412@gmail.com>
2025-04-04 01:25:46 -0500

    Add files via upload

---
+++ overviewerConfig.js
@@ -0,0 +1,101 @@
+var overviewerConfig = {
+    "CONST": {
+        "tileSize": 384,
+        "image": {
+            "defaultMarker": "signpost.png",
+            "signMarker": "signpost_icon.png",
+            "bedMarker": "bed.png",
+            "spawnMarker": "markers/marker_home.png",
+            "spawnMarker2x": "markers/marker_home_2x.png",
+            "queryMarker": "markers/marker_location.png",
+            "queryMarker2x": "markers/marker_location_2x.png",
+            "compass": {
+                "0": "compass_upper-left.png",
+                "1": "compass_upper-right.png",
+                "3": "compass_lower-left.png",
+                "2": "compass_lower-right.png"
+            }
+        },
+        "mapDivId": "mcmap",
+        "UPPERLEFT": 0,
+        "UPPERRIGHT": 1,
+        "LOWERLEFT": 3,
+        "LOWERRIGHT": 2
+    },
+    "worlds": [
+        "TacoBell"
+    ],
+    "map": {
+        "debug": false,
+        "cacheTag": "1743747423",
+        "north_direction": "lower-left",
+        "controls": {
+            "pan": true,
+            "zoom": true,
+            "spawn": true,
+            "compass": true,
+            "mapType": true,
+            "overlays": true,
+            "coordsBox": true
+        },
+        "attribution": null
+    },
+    "tilesets": [
+        {
+            "name": "Taco Bell Cantina - Day",
+            "zoomLevels": 7,
+            "defaultZoom": 1,
+            "maxZoom": 7,
+            "path": "day",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "TacoBell",
+            "last_rendertime": 1743402941,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                -338,
+                89,
+                104
+            ],
+            "lastrenderversion": 2,
+            "minZoom": 0,
+            "spawn": [
+                -338,
+                89,
+                104
+            ],
+            "north_direction": 0
+        },
+        {
+            "name": "Taco Bell Cantina - Night",
+            "zoomLevels": 7,
+            "defaultZoom": 1,
+            "maxZoom": 7,
+            "path": "night",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "TacoBell",
+            "last_rendertime": 1743402941,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                -338,
+                89,
+                104
+            ],
+            "lastrenderversion": 2,
+            "minZoom": 0,
+            "spawn": [
+                -338,
+                89,
+                104
+            ],
+            "north_direction": 0
+        }
+    ]
+};
